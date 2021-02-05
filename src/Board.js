0c86096229231b9888f0fcc9e8704e27c82b9f51
Sandra Spanikova <spanikovasandra@gmail.com>
2021-02-05 14:29:32 +0100

    implement game logic

--- src/Board.js
+++ src/Board.js
@@ -27,34 +27,30 @@ import "./Board.css";
  *
  **/
 
-const Board = ({ nrows, ncols, chanceLightStartsOn }) => {
-
+function Board({ nrows = 5, ncols = 5, chanceLightStartsOn = 0.25 }) {
   const [board, setBoard] = useState(createBoard());
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard() {
     let initialBoard = [];
-
-    for (let i = 0; i < ncols; i++) {
-      initialBoard.push([]);
-      for (let j = 0; j < nrows; j++) {
-        initialBoard[i].push(Math.random());
+    for (let y = 0; y < nrows; y++) {
+      let row = [];
+      for (let x = 0; x < ncols; x++) {
+        row.push(Math.random() < chanceLightStartsOn);
       }
+      initialBoard.push(row);
     }
-
-    const trueFalseBoard = initialBoard.map((arr) =>
-      arr.map((num) => (num > chanceLightStartsOn ? true : false))
-    );
-
-    return trueFalseBoard;
+    return initialBoard;
   }
 
+  /* Check if the player has won */
   function hasWon() {
-    // TODO: check the board in state to determine whether the player has won.
+    return board.every(row => row.every(cell => !cell));
   }
 
+  /* Flip cells around a given cell */
   function flipCellsAround(coord) {
-    setBoard((oldBoard) => {
+    setBoard(oldBoard => {
       const [y, x] = coord.split("-").map(Number);
 
       const flipCell = (y, x, boardCopy) => {
@@ -65,11 +61,15 @@ const Board = ({ nrows, ncols, chanceLightStartsOn }) => {
         }
       };
 
-      // TODO: Make a (deep) copy of the oldBoard
+      const boardCopy = oldBoard.map(row => [...row]);
 
-      // TODO: in the copy, flip this cell and the cells around it
+      flipCell(y, x, boardCopy);
+      flipCell(y, x - 1, boardCopy);
+      flipCell(y, x + 1, boardCopy);
+      flipCell(y - 1, x, boardCopy);
+      flipCell(y + 1, x, boardCopy);
 
-      // TODO: return the copy
+      return boardCopy;
     });
   }
 
@@ -78,32 +78,30 @@ const Board = ({ nrows, ncols, chanceLightStartsOn }) => {
     return <div>You Win!</div>;
   }
 
-  // make table board
-  let tableBoard = []; 
+  // make table board: rows of Cell components
 
-  for (let y = 0; y < ncols; y++) {
-    let row = []; 
-    for (let z = 0; z < nrows; z++) {
-      let coord = `${z}-${y}`; 
+  let tblBoard = [];
+
+  for (let y = 0; y < nrows; y++) {
+    let row = [];
+    for (let x = 0; x < ncols; x++) {
+      let coord = `${y}-${x}`;
       row.push(
-        <Cell 
-        key={coord}
-        isLit={board[y][z]}
-        flipCellsAroundMe={() => flipCellsAround(coord)}
+        <Cell
+          key={coord}
+          isLit={board[y][x]}
+          flipCellsAroundMe={() => flipCellsAround(coord)}
         />
-      )
+      );
     }
-    tableBoard.push(<tr key={y}>{row}</tr>)
+    tblBoard.push(<tr key={y}>{row}</tr>);
   }
 
-
   return (
-    <div className='Board'>
-      <tbody>{tableBoard}</tbody>
-    </div>
-  )
-};
-
-
+    <table className="Board">
+      <tbody>{tblBoard}</tbody>
+    </table>
+  );
+}
 
 export default Board;
